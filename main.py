af857895bf58e4a74a07db266517279c1e439bdc
LawMixer <53837083+LawMixer@users.noreply.github.com>
2022-06-27 14:06:33 -0400

    replaced with brand new api [working]

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-# pip install Flask
-# pip install firebase-admin
+from flask import Flask
+from flask import jsonify
+import requests
+app = Flask('app')
 
-from api import create_app
+discordId = 247036279420747776
+money = 100
 
-app = create_app
+req1 = requests.get(f"https://verify.eryn.io/api/user/{discordId}")
+robloxUsername = req1.json()["robloxUsername"]
 
-if __name__ == "__main__":
-    app.run(debug = True)
+req2 = requests.get(f"https://verify.eryn.io/api/user/{discordId}")
+robloxId = req2.json()["robloxId"]
+
+# https://lk.policelaw14.repl.co/users/247036279420747776
+# api.lawmixerscpf.tk/users/{discordId}
+@app.route(f"/users/{discordId}")
+def get_roblox_info():
+  return jsonify(f"money: {money}, robloxUsername: {robloxUsername}, robloxId: {robloxId}")
+
+
+app.run(host='0.0.0.0', port=8080)
